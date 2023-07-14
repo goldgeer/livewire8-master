1f9c371f60e318aeebe1d9fd513d0fa8d6e2f1a6
goldgeer <goldgeer@gmail.com>
2023-07-14 09:52:12 +0000

    add flowbite

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div class="p-4 max-w-5xl grid gap-4">
+    <h1 class="text-4xl font-extrabold">Grid layout with Tailwind CSS.</h1>
+    <p>
+        In this video, you'll learn how to optimize your SQL queries by understanding the SQL execution order. We'll cover the different steps involved in executing a SQL query, and how you can use this knowledge to improve the performance of your queries.
+
+    </p>
+</div>
